d85a956440b9bb7a689de57f4982324dd4ea105b
ismael <ismaeljaycarpio1992@gmail.com>
2018-05-28 23:05:05 +0800

    added add/edit/delete for pages

--- CmsShoppingCart/Areas/Admin/Controllers/PagesController.cs
+++ CmsShoppingCart/Areas/Admin/Controllers/PagesController.cs
@@ -77,7 +77,7 @@ namespace CmsShoppingCart.Areas.Admin.Controllers
                 dto.Slug = slug;
                 dto.Body = model.Body;
                 dto.HasSideBar = model.HasSideBar;
-                dto.Sorting = model.Sorting;
+                dto.Sorting = 100;
 
                 // save dto
                 db.Pages.Add(dto);
@@ -90,6 +90,108 @@ namespace CmsShoppingCart.Areas.Admin.Controllers
                 return RedirectToAction("AddPage");
             }
             
+        }
+
+        // GET: admin/pages/editpage/id
+        public ActionResult EditPage(int id)
+        {
+            // declare pagevm
+            PageViewModel model;
+
+            using(CmsShoppingCartDb db = new CmsShoppingCartDb())
+            {
+                // get the page
+                PageDTO dto = db.Pages.Find(id);
+
+                //confirm page exists
+                if(dto == null)
+                {
+                    return Content("The page does not exist.");
+                }
+
+                // init page
+                model = new PageViewModel(dto);
+            }
+
+             // return vm
+            return View(model);
+        }
+
+        // POST: admin/pages/editpage/id
+        [HttpPost]
+        public ActionResult EditPage(PageViewModel vm)
+        {
+            // get page id
+            int id = vm.Id;
+
+            // check model state
+            if(! ModelState.IsValid)
+            {
+                return View(vm);
+            }
+
+            using(CmsShoppingCartDb db = new CmsShoppingCartDb())
+            {
+                string slug = "";
+
+                // get the page
+                PageDTO dto = db.Pages.Find(id);
+
+                dto.Title = vm.Title;
+
+                if(vm.Slug != "home")
+                {
+                    if(string.IsNullOrWhiteSpace(vm.Slug))
+                    {
+                        slug = vm.Title.Replace(" ", "-").ToLower();
+                    }
+                    else
+                    {
+                        slug = vm.Slug.Replace(" ", "-").ToLower();
+                    }
+                }
+
+                // title and slug are unique
+                if(db.Pages.Where(x => x.Id != id).Any(x => x.Title == vm.Title) || 
+                    db.Pages.Where(x => x.Id != id).Any(x => x.Slug == vm.Slug))
+                {
+                    ModelState.AddModelError("", "The Title or Slug already exists");
+                    return View(vm);
+                }
+
+                // update dto
+                dto.Slug = slug;
+                dto.Body = vm.Body;
+                dto.Sorting = vm.Sorting;
+                dto.HasSideBar = vm.HasSideBar;
+
+                // save dto
+                db.SaveChanges();
+            }
+
+            // set tempdate
+            TempData["SuccessMessage"] = "page udated!";
+
+            return RedirectToAction("EditPage", new { Id = id});
+        }
+
+        // GET: admin/pages/DeletePage/id
+        public ActionResult DeletePage(int id)
+        {
+            // db
+            using(CmsShoppingCartDb db = new CmsShoppingCartDb())
+            {
+                // get page
+                PageDTO dto = db.Pages.Find(id);
+
+                // remove page
+                db.Pages.Remove(dto);
+
+                // save
+                db.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
         }
 
     }
